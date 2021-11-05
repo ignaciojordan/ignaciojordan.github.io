3811943920cc1759cb156aaa62a3d3547714c0af
ignaciojordan <ignaciojordan96@gmail.com>
2021-11-05 17:54:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,15 @@ $(document).ready(function() {
 
         event.preventDefault(); // prevent PageReLoad
 
-       var ValidEmail = $('#username').val() === 'ignaciojordan96'; // User validate
+       var ValidEmail = $('#username').val() === 'ijordan'; // User validate
 var ValidPassword = $('#password').val() === '4252'; // Password validate
 
         if (ValidEmail === true && ValidPassword === true) { // if ValidEmail & ValidPassword
             $('.valid').css('display', 'block');
-            close_window("https://ignaciojordan.github.io/");
+            let delayInMilliseconds = 5000; //1 second
+            setTimeout(function() {
+                close_window("https://ignaciojordan.github.io/");//your code to be executed after 1 second
+            }, delayInMilliseconds);
         }
         else {
             $('.error').css('display', 'block'); // show error msg
