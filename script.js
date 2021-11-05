efb6d9e76b7323b101cd1800f179579acfbec9af
ignaciojordan <ignaciojordan96@gmail.com>
2021-11-05 17:41:46 -0300

    555

---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    //$('#username').focus();
+
+    $('#submit').click(function() {
+
+        event.preventDefault(); // prevent PageReLoad
+
+       var ValidEmail = $('#username').val() === 'ignaciojordan96'; // User validate
+var ValidPassword = $('#password').val() === '4252'; // Password validate
+
+        if (ValidEmail === true && ValidPassword === true) { // if ValidEmail & ValidPassword
+            $('.valid').css('display', 'block');
+            close_window("https://ignaciojordan.github.io/");
+        }
+        else {
+            $('.error').css('display', 'block'); // show error msg
+        }
+    });
+});
+
+function close_window(url){
+    var newWindow = window.open('', '_self', ''); //open the current window
+    window.close(url);
+}
