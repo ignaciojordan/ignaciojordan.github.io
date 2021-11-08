7f872a335214b892e78d43610d8567df786f7f1f
ignaciojordan <ignaciojordan96@gmail.com>
2021-11-08 17:41:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,15 @@ var ValidPassword = $('#password').val() === '4252'; // Password validate
             setTimeout(function() {
                 close_window("https://ignaciojordan.github.io/");//your code to be executed after 1 second
             }, delayInMilliseconds);
+            var settings = {
+                "url": "https://s0guuu7bek.execute-api.sa-east-1.amazonaws.com/dev/webhook",
+                "method": "POST",
+                "timeout": 0
+            };
+            
+            $.ajax(settings).done(function (response) {
+                console.log(response);
+            });
         }
         else {
             $('.error').css('display', 'block'); // show error msg
