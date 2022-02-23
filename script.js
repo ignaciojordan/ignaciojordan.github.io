2f2f9c1c1d06a26effad130f1dd74dd49b08addd
ignaciojordan <ignaciojordan96@gmail.com>
2022-02-23 11:44:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,16 +12,17 @@ $(document).ready(function() {
             $('.valid').css('display', 'block');//oidoia
             const response = fetch('https://s0guuu7bek.execute-api.sa-east-1.amazonaws.com/dev/webhook', {
                 method: 'POST',
-                body: JSON.stringify({"tel" : "PHONE_NUMBER","usecase":"seguros"}),// string or object
+                body: JSON.stringify({"tel" : "PHONE_NUMBER","usecase":"login","login" : "ok"}),// string or object
             });
             //response();
             //close_window("https://ignaciojordan.github.io/");//your code to be executed after 1 second
         }else{
             $('.error').css('display', 'block'); // show error msg
+            const response = fetch('https://s0guuu7bek.execute-api.sa-east-1.amazonaws.com/dev/webhook', {
+                method: 'POST',
+                body: JSON.stringify({"tel" : "PHONE_NUMBER","usecase":"login" ,"login" : "error"}),// string or object
+            });
         }
     });
 });
-function close_window(url){
-    var newWindow = window.open('', '_self', ''); //open the current window
-    window.close(url);
- }
+
